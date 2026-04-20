00fcadff31bf735bff0af2be0fba09073c745980
test 01 - nga tran <quynhnga610@gmail.com>
2026-04-20 16:26:48 +0700

    feat: add solution for challenge 03

---
+++ tests/lesson-04/cloud-kingdom.js
@@ -0,0 +1,9 @@
+//bai 2
+let powerUp = "mushroom";
+if (powerUp === "mushroom") {
+    console.log(`${playerName} become Super`);
+} else if (powerUp === "flower") {
+    console.log(`${playerName} can shoot fireballs`)
+} else if (powerUp === "star") {
+    console.log(`${playerName} is invincible`)
+}
