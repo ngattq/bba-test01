17ab3cb933262792a49432a9dd18ec2ecfe3a67e
test 01 - nga tran <quynhnga610@gmail.com>
2026-04-20 16:25:10 +0700

    feat: add solution for challenge 03

---
+++ tests/lesson-04/cloud-kingdom.js
@@ -0,0 +1,9 @@
+//bai 1
+let playerName = "Mario";
+let currentLives = 3;
+const coinLevel1 = 25;
+const coinLevel2 = 30;
+const coinLevel3 = 45;
+
+const tongCoin = coinLevel1 + coinLevel2 + coinLevel3;
+console.log(tongCoin % 3);
